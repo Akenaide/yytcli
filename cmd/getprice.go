75b2efa0f5c1c82846054f9c1983524e035d6d57
Kevin MS <kevin.mansang@mhcomm.fr>
2018-02-02 15:34:38 +0100

    Add new cmd to calculate playset on yyt price

---
+++ cmd/getprice.go
@@ -0,0 +1,50 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	mylib "gogs.ded.eelon.moe/kms/yytcli/libs"
+)
+
+var goodRarity = []string{"RR", "R", "U", "C"}
+
+// getpriceCmd represents the getprice command
+var getpriceCmd = &cobra.Command{
+	Use:   "getprice",
+	Short: "A brief description of your command",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("getprice called")
+		for _, serie := range Series {
+			var price = 0
+			cardMap := mylib.GetCards([]string{serie})
+			for _, infos := range cardMap {
+				for _, rarity := range goodRarity {
+					if rarity == infos.Rarity {
+						price = price + (infos.Price * 4)
+					}
+				}
+			}
+			fmt.Printf("%v: %v\n", serie, price)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getpriceCmd)
+}
